59bd5ad9ee558572d5175b493bee3c764beb4895
Viktor Walter <vi.wa.kgt@gmail.com>
2022-04-18 10:30:51 +0200

    init

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 from dfeditm import dfedit
 
-root = '/Users/viktorwalter/Library/Mobile Documents/com~apple~CloudDocs/Studium/Wirtschaftsingenieurwesen B.Sc./8. Semester SS22/Thesis/Praxis/Database/database_0416.db'
+root = ''
 
 df = dfedit.df_edit(root)
 
