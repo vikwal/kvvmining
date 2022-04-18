775e50c133dea8aa0dd7ba3b4f313b0336e0fcb6
Viktor Walter <vi.wa.kgt@gmail.com>
2022-04-18 10:30:04 +0200

    init

--- dfeditm/dfedit.py
+++ dfeditm/dfedit.py
@@ -8,13 +8,33 @@ def create_df(table, root):
   return df
 
 #Funktion, die einem Dataframe die Verspätung in Sekunden hinzufügt sowie Namen für die Haltestellen
-def df_edit(df, dfstops):
+def df_edit(root):
     #Zuerst erstellt man aus allen Tabellen der Datenbank jeweils einen Dataframe
-    start_delay = (df['start_ist'] - df['start_soll']).dt.total_seconds()
-    end_delay = (df['end_ist'] - df['end_soll']).dt.total_seconds()
-    dfstops = dfstops.drop(['StartID', 'EndID'], axis = 1)
-
+    df0 = create_df('Fahrtanfragen', root)
+    dflines = create_df('Linien', root)
+    dfroute = create_df('RouteStops', root)
+    dfstops = create_df('stopPointList', root)
+    #Dann werden die Dataframes bearbeitet
+    start_delay = (df0['start_ist'] - df0['start_soll']).dt.total_seconds()
+    end_delay = (df0['end_ist'] - df0['end_soll']).dt.total_seconds()
+    dflines = dflines.drop(['StartID', 'EndID'], axis = 1)
+    dfroute1 = dfroute.rename(columns={'Station': "start"})
+    dfroute1 = dfroute1.drop_duplicates(subset=['start', 'line_ref','route'])
+    dfroute2 = dfroute.rename(columns={'Station': "end"})
+    dfroute2 = dfroute2.drop_duplicates(subset=['end', 'line_ref','route'])
+    dfstops1 = dfstops.rename(columns={'Station': "start"})
+    dfstops2 = dfstops.rename(columns={'Station': "end"})
     #Verbinden der Dataframes zu einem großen sowie Umbenennung der Spalten, falls nötig
-    df1 = pd.concat([df, start_delay, end_delay], axis=1, join="inner")
-    df1 = df1.rename(columns={0: "start_delay", 1: "end_delay"})
-    return df1
+    df = pd.concat([df0, start_delay, end_delay], axis=1)
+    df = df.rename(columns={0: "start_delay", 1: "end_delay"})
+    df = pd.merge(df, dflines, on='line_ref', how='left')
+    df = pd.merge(df, dfroute1, on=['start', 'line_ref','route'], how='left')
+    df = df.rename(columns={'StopSeq': "StartSeq"})
+    df = pd.merge(df, dfroute2, on=['end', 'line_ref','route', 'Direction'], how='left')
+    df = df.rename(columns={'StopSeq': "EndSeq"})
+    df = pd.merge(df, dfstops1, on='start', how='left')
+    df = df.rename(columns={'StopPointName': 'StartName', 'LocationName': 'StartLocation', 'Longitude': 'StartLong', 'Latitude': 'StartLat'})
+    df = pd.merge(df, dfstops2, on='end', how='left')
+    df = df.rename(columns={'StopPointName': 'EndName', 'LocationName': 'EndLocation', 'Longitude': 'EndLong', 'Latitude': 'EndLat'})    
+    #df = df[df['start'].notna()] #Löscht Zeilen, die Na-Werte enthalten (NaN, NaT)
+    return df
--- test.py
+++ test.py
@@ -1,32 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from dfeditm import dfedit
 
-#Erstellt einen Dataframe aus einem sqlite database file
-def create_df(table, root):
-  engine = create_engine('sqlite:///' + root)
-  df = pd.read_sql(table, engine)                              
-  return df
+root = '/Users/viktorwalter/Library/Mobile Documents/com~apple~CloudDocs/Studium/Wirtschaftsingenieurwesen B.Sc./8. Semester SS22/Thesis/Praxis/Database/database_0416.db'
 
-#Funktion, die einem Dataframe die Verspätung in Sekunden hinzufügt sowie Namen für die Haltestellen
-def df_edit(root):
-    #Zuerst erstellt man aus allen Tabellen der Datenbank jeweils einen Dataframe
-    df0 = create_df('Fahrtanfragen', root)
-    dflines = create_df('Linien', root)
-    dfroute = create_df('RouteStops', root)
-    dfstops = create_df('stopPointList', root)
-    #Dann werden die Dataframes bearbeitet
-    start_delay = (df0['start_ist'] - df0['start_soll']).dt.total_seconds()
-    end_delay = (df0['end_ist'] - df0['end_soll']).dt.total_seconds()
-    dflines = dflines.drop(['StartID', 'EndID'], axis = 1)
-    dflines = dflines.rename(columns={'LineRef': "line_ref"})
-    #Verbinden der Dataframes zu einem großen sowie Umbenennung der Spalten, falls nötig
-    df = pd.concat([df0, start_delay, end_delay], axis=1)
-    df = df.rename(columns={0: "start_delay", 1: "end_delay"})
-    return df
-
-root = ''
-
-df = df_edit(root)
-df = df.drop(['timestamp', 'start', 'end', 'start_soll', 'start_ist', 'end_soll', 'end_ist'], axis = 1)
+df = dfedit.df_edit(root)
 
 print(df)
